386f6814af29a08a02e767c6bad867dee6a1e271
ALAMAP\lisiqi <lisiqi@edspay.com>
2018-10-17 18:15:24 +0800

    aop配置

--- src/main/java/com/pro47x/demo/TestApp.java
+++ src/main/java/com/pro47x/demo/TestApp.java
@@ -12,7 +12,7 @@ public class TestApp {
     public void demo01() {
         String xmlPath = "applicationContext.xml";
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
-        AccountService accountService = (AccountService) applicationContext.getBean("proxyAccountService");
+        AccountService accountService = (AccountService) applicationContext.getBean("accountService ");
         accountService.transfer("jack", "rose", 1000);
     }
 }
